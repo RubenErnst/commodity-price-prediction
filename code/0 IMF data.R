d541a11bcd3ac46c6949e6f4c792bea54f71f9c7
Ruben Ernst <57605025+RubenErnst@users.noreply.github.com>
2021-03-20 00:52:06 +0100

    Update 0 IMF data.R

--- code/0 IMF data.R
+++ code/0 IMF data.R
@@ -7,3 +7,11 @@ library(IMFData)
 
 
 # Laod IMF data
+oil.production.index <- CompactDataMethod("AOMPC_IX")
+oil.production.perc.yoy <- CompactDataMethod("AOMPC_PC_CP_A_PT")
+oil.production.perc.previous <- CompactDataMethod("AOMPC_PC_PP_PT")
+
+commodities.prices.export <- CompactDataMethod("PXP_IX")
+commodities.prices.import <- CompactDataMethod("PMP_IX")
+commodities.prices.producer <- CompactDataMethod("PPPI_IX")
+commodities.prices.industrial.producer <- CompactDataMethod("PPPII_IX")
