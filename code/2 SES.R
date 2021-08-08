8f1b02a0477241528c39743cdd36f05424b5a58f
Ruben Ernst <57605025+RubenErnst@users.noreply.github.com>
2021-08-08 20:53:25 +0200

    Update 2 SES.R

--- code/2 SES.R
+++ code/2 SES.R
@@ -19,34 +19,76 @@ mae <- function(y, pred){
 }
 
 # Resulting data frame
-result <- data.frame("commodity" = rep(c("APSP", "Brent", "Dubai", "US NatGas", "WTI"), 2),
+ses.results <- data.frame("commodity" = rep(c("APSP", "Brent", "Dubai", "US NatGas", "WTI"), 2),
                      "series" = c(rep("difference", 5), rep("log returns", 5)),
                      "mae" = rep(NA, 10),
                      "mape" = rep(NA, 10))
 
 ### APSP
-ses.apsp.monthly.difference <- forecast::ses(ts.apsp.monthly.difference, h = 12) # h determines the number of periods to be forecast
-ses.apsp.monthly.log.returns <- forecast::ses(ts.apsp.monthly.log.returns, h = 12)
+ses.apsp.monthly.difference <- forecast::ses(ts.apsp.monthly.difference[1 : (length(ts.apsp.monthly.difference) - 11)], h = 12) # h determines the number of periods to be forecast
+ses.apsp.monthly.log.returns <- forecast::ses(ts.apsp.monthly.log.returns[1 : (length(ts.apsp.monthly.log.returns) - 11)], h = 12)
 
-result$mae[1] <- mae(ts.apsp.monthly.difference[(length(ts.apsp.monthly.difference) - 11): length(ts.apsp.monthly.difference)],
+ses.results$mae[1] <- mae(ts.apsp.monthly.difference[(length(ts.apsp.monthly.difference) - 11): length(ts.apsp.monthly.difference)],
                      as.data.frame(ses.apsp.monthly.difference)$`Point Forecast`)
-result$mae[6] <- mae(ts.apsp.monthly.log.returns[(length(ts.apsp.monthly.log.returns) - 11): length(ts.apsp.monthly.log.returns)],
+ses.results$mae[6] <- mae(ts.apsp.monthly.log.returns[(length(ts.apsp.monthly.log.returns) - 11): length(ts.apsp.monthly.log.returns)],
                      as.data.frame(ses.apsp.monthly.log.returns)$`Point Forecast`)
-result$mape[1] <- mape(ts.apsp.monthly.difference[(length(ts.apsp.monthly.difference) - 11): length(ts.apsp.monthly.difference)],
+ses.results$mape[1] <- mape(ts.apsp.monthly.difference[(length(ts.apsp.monthly.difference) - 11): length(ts.apsp.monthly.difference)],
                       as.data.frame(ses.apsp.monthly.difference)$`Point Forecast`)
-result$mape[6] <- mape(ts.apsp.monthly.log.returns[(length(ts.apsp.monthly.log.returns) - 11): length(ts.apsp.monthly.log.returns)],
+ses.results$mape[6] <- mape(ts.apsp.monthly.log.returns[(length(ts.apsp.monthly.log.returns) - 11): length(ts.apsp.monthly.log.returns)],
                       as.data.frame(ses.apsp.monthly.log.returns)$`Point Forecast`)
 
 ### Brent
-ses.brent.monthly.difference <- forecast::ses(ts.brent.monthly.difference, h = 12) # h determines the number of periods to be forecast
-ses.brent.monthly.log.returns <- forecast::ses(ts.brent.monthly.log.returns, h = 12)
+ses.brent.monthly.difference <- forecast::ses(ts.brent.monthly.difference[1: (length(ts.brent.monthly.difference) - 11)], h = 12)
+ses.brent.monthly.log.returns <- forecast::ses(ts.brent.monthly.log.returns[1: (length(ts.brent.monthly.log.returns) - 11)], h = 12)
 
-result$mae[2] <- mae(ts.brent.monthly.difference[(length(ts.brent.monthly.difference) - 11): length(ts.brent.monthly.difference)],
+ses.results$mae[2] <- mae(ts.brent.monthly.difference[(length(ts.brent.monthly.difference) - 11): length(ts.brent.monthly.difference)],
                      as.data.frame(ses.brent.monthly.difference)$`Point Forecast`)
-result$mae[7] <- mae(ts.brent.monthly.log.returns[(length(ts.brent.monthly.log.returns) - 11): length(ts.brent.monthly.log.returns)],
+ses.results$mae[7] <- mae(ts.brent.monthly.log.returns[(length(ts.brent.monthly.log.returns) - 11): length(ts.brent.monthly.log.returns)],
                      as.data.frame(ses.brent.monthly.log.returns)$`Point Forecast`)
-result$mape[2] <- mape(ts.brent.monthly.difference[(length(ts.brent.monthly.difference) - 11): length(ts.brent.monthly.difference)],
+ses.results$mape[2] <- mape(ts.brent.monthly.difference[(length(ts.brent.monthly.difference) - 11): length(ts.brent.monthly.difference)],
                        as.data.frame(ses.brent.monthly.difference)$`Point Forecast`)
-result$mape[7] <- mape(ts.brent.monthly.log.returns[(length(ts.brent.monthly.log.returns) - 11): length(ts.brent.monthly.log.returns)],
+ses.results$mape[7] <- mape(ts.brent.monthly.log.returns[(length(ts.brent.monthly.log.returns) - 11): length(ts.brent.monthly.log.returns)],
                        as.data.frame(ses.brent.monthly.log.returns)$`Point Forecast`)
 
+### Dubai
+ses.dubai.monthly.difference <- forecast::ses(ts.dubai.monthly.difference[1: (length(ts.dubai.monthly.difference) - 11)], h = 12)
+ses.dubai.monthly.log.returns <- forecast::ses(ts.dubai.monthly.log.returns[1: (length(ts.dubai.monthly.log.returns) - 11)], h = 12)
+
+ses.results$mae[3] <- mae(ts.dubai.monthly.difference[(length(ts.dubai.monthly.difference) - 11): length(ts.dubai.monthly.difference)],
+                     as.data.frame(ses.dubai.monthly.difference)$`Point Forecast`)
+ses.results$mae[8] <- mae(ts.dubai.monthly.log.returns[(length(ts.dubai.monthly.log.returns) - 11): length(ts.dubai.monthly.log.returns)],
+                     as.data.frame(ses.dubai.monthly.log.returns)$`Point Forecast`)
+ses.results$mape[3] <- mape(ts.dubai.monthly.difference[(length(ts.dubai.monthly.difference) - 11): length(ts.dubai.monthly.difference)],
+                       as.data.frame(ses.dubai.monthly.difference)$`Point Forecast`)
+ses.results$mape[8] <- mape(ts.dubai.monthly.log.returns[(length(ts.dubai.monthly.log.returns) - 11): length(ts.dubai.monthly.log.returns)],
+                       as.data.frame(ses.dubai.monthly.log.returns)$`Point Forecast`)
+
+### NatGas
+ses.natgas.us.monthly.difference <- forecast::ses(ts.natgas.us.monthly.difference[1: (length(ts.natgas.us.monthly.difference) - 11)], h = 12)
+ses.natgas.us.monthly.log.returns <- forecast::ses(ts.natgas.us.monthly.log.returns[1: (length(ts.natgas.us.monthly.log.returns) - 11)], h = 12)
+
+ses.results$mae[4] <- mae(ts.natgas.us.monthly.difference[(length(ts.natgas.us.monthly.difference) - 11): length(ts.natgas.us.monthly.difference)],
+                     as.data.frame(ses.natgas.us.monthly.difference)$`Point Forecast`)
+ses.results$mae[9] <- mae(ts.natgas.us.monthly.log.returns[(length(ts.natgas.us.monthly.log.returns) - 11): length(ts.natgas.us.monthly.log.returns)],
+                     as.data.frame(ses.natgas.us.monthly.log.returns)$`Point Forecast`)
+ses.results$mape[4] <- mape(ts.natgas.us.monthly.difference[(length(ts.natgas.us.monthly.difference) - 11): length(ts.natgas.us.monthly.difference)],
+                       as.data.frame(ses.natgas.us.monthly.difference)$`Point Forecast`)
+ses.results$mape[9] <- mape(ts.natgas.us.monthly.log.returns[(length(ts.natgas.us.monthly.log.returns) - 11): length(ts.natgas.us.monthly.log.returns)],
+                       as.data.frame(ses.natgas.us.monthly.log.returns)$`Point Forecast`)
+
+### WTI
+ses.wti.monthly.difference <- forecast::ses(ts.wti.monthly.difference[1: (length(ts.wti.monthly.difference) - 11)], h = 12)
+ses.wti.monthly.log.returns <- forecast::ses(ts.wti.monthly.log.returns[1: (length(ts.wti.monthly.log.returns) - 11)], h = 12)
+
+ses.results$mae[5] <- mae(ts.wti.monthly.difference[(length(ts.wti.monthly.difference) - 11): length(ts.wti.monthly.difference)],
+                     as.data.frame(ses.wti.monthly.difference)$`Point Forecast`)
+ses.results$mae[10] <- mae(ts.wti.monthly.log.returns[(length(ts.wti.monthly.log.returns) - 11): length(ts.wti.monthly.log.returns)],
+                     as.data.frame(ses.wti.monthly.log.returns)$`Point Forecast`)
+ses.results$mape[5] <- mape(ts.wti.monthly.difference[(length(ts.wti.monthly.difference) - 11): length(ts.wti.monthly.difference)],
+                       as.data.frame(ses.wti.monthly.difference)$`Point Forecast`)
+ses.results$mape[10] <- mape(ts.wti.monthly.log.returns[(length(ts.wti.monthly.log.returns) - 11): length(ts.wti.monthly.log.returns)],
+                       as.data.frame(ses.wti.monthly.log.returns)$`Point Forecast`)
+
+
+### Save the results
+save(ses.results, file = "results/econometric models/SES.RData")
