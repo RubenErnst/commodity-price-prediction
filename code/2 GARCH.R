9622a56eab9a148a427f433f62650d5a3333fd4a
Ruben Ernst <57605025+RubenErnst@users.noreply.github.com>
2021-08-20 14:54:52 +0200

    Update 2 GARCH.R

--- code/2 GARCH.R
+++ code/2 GARCH.R
@@ -1,1 +1,15 @@
 rm(list = ls())
+
+library(tidyverse)
+library(forecast)
+library(rugarch)
+
+
+model <- ugarchspec(mean.model = list(armaOrder=c(0, 0), include.mean = FALSE), distribution = "std")
+fit <- ugarchfit(spec = model, ts.apsp.monthly.log.returns)
+coef(fit)
+plot(sqrt(252) * fit@fit$sigma, type='l')
+
+spec <- getspec(fit)
+setfixed(spec) <- as.list(coef(fit))
+forecast <- ugarchforecast(spec, n.ahead = 12, n.roll = 362, data = ts.apsp.monthly.log.returns[1:374,drop=FALSE], out.sample = 362)
