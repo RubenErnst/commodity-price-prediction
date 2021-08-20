bfb479fabeaf030b46c430e02b54ad25ff760cb5
Ruben Ernst <57605025+RubenErnst@users.noreply.github.com>
2021-08-20 14:55:03 +0200

    Update SES

--- code/2 SES.R
+++ code/2 SES.R
@@ -18,77 +18,219 @@ mae <- function(y, pred){
   return(out)
 }
 
+# Dealing with ts times
+conv_to_ym <- function(ts_date){
+  year <- floor(ts_date)
+  month <- ts_date - year
+  return(c(year, round(month * 12) + 1))
+}
+
+
 # Resulting data frame
 ses.results <- data.frame("commodity" = rep(c("APSP", "Brent", "Dubai", "US NatGas", "WTI"), 2),
-                     "series" = c(rep("difference", 5), rep("log returns", 5)),
-                     "mae" = rep(NA, 10),
-                     "mape" = rep(NA, 10))
+                          "series" = c(rep("difference", 5), rep("log returns", 5)),
+                          "alpha" = rep(NA, 10),
+                          "mae" = rep(NA, 10),
+                          "mape" = rep(NA, 10))
 
-### APSP
-ses.apsp.monthly.difference <- forecast::ses(ts.apsp.monthly.difference[1 : (length(ts.apsp.monthly.difference) - 11)], h = 12) # h determines the number of periods to be forecast
-ses.apsp.monthly.log.returns <- forecast::ses(ts.apsp.monthly.log.returns[1 : (length(ts.apsp.monthly.log.returns) - 11)], h = 12)
+### APSP first difference
+# Split data
+ts.train <- ts(ts.apsp.monthly.difference[1:(length(ts.apsp.monthly.difference) - 12)],
+               start = conv_to_ym(time(ts.apsp.monthly.difference)[1] - 0.083),
+               end = conv_to_ym(time(ts.apsp.monthly.difference)[(length(ts.apsp.monthly.difference) - 12)] - 0.083),
+               frequency = 12)
+ts.test <- ts(ts.apsp.monthly.difference[(length(ts.apsp.monthly.difference) - 11):length(ts.apsp.monthly.difference)],
+              start = conv_to_ym(time(ts.apsp.monthly.difference)[(length(ts.apsp.monthly.difference) - 11)] - 0.083),
+              end = conv_to_ym(time(ts.apsp.monthly.difference)[length(ts.apsp.monthly.difference)] - 0.083),
+              frequency = 12)
 
-ses.results$mae[1] <- mae(ts.apsp.monthly.difference[(length(ts.apsp.monthly.difference) - 11): length(ts.apsp.monthly.difference)],
-                     as.data.frame(ses.apsp.monthly.difference)$`Point Forecast`)
-ses.results$mae[6] <- mae(ts.apsp.monthly.log.returns[(length(ts.apsp.monthly.log.returns) - 11): length(ts.apsp.monthly.log.returns)],
-                     as.data.frame(ses.apsp.monthly.log.returns)$`Point Forecast`)
-ses.results$mape[1] <- mape(ts.apsp.monthly.difference[(length(ts.apsp.monthly.difference) - 11): length(ts.apsp.monthly.difference)],
-                      as.data.frame(ses.apsp.monthly.difference)$`Point Forecast`)
-ses.results$mape[6] <- mape(ts.apsp.monthly.log.returns[(length(ts.apsp.monthly.log.returns) - 11): length(ts.apsp.monthly.log.returns)],
-                      as.data.frame(ses.apsp.monthly.log.returns)$`Point Forecast`)
+# Fit SES model
+fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
+ses.results$alpha[1] <- unname(fit$model$par[1])
 
-### Brent
-ses.brent.monthly.difference <- forecast::ses(ts.brent.monthly.difference[1: (length(ts.brent.monthly.difference) - 11)], h = 12)
-ses.brent.monthly.log.returns <- forecast::ses(ts.brent.monthly.log.returns[1: (length(ts.brent.monthly.log.returns) - 11)], h = 12)
+# Evaluate
+ses.results$mae[1] <- mae(ts.test, fit$mean)
+ses.results$mape[1] <- mape(ts.test, fit$mean)
 
-ses.results$mae[2] <- mae(ts.brent.monthly.difference[(length(ts.brent.monthly.difference) - 11): length(ts.brent.monthly.difference)],
-                     as.data.frame(ses.brent.monthly.difference)$`Point Forecast`)
-ses.results$mae[7] <- mae(ts.brent.monthly.log.returns[(length(ts.brent.monthly.log.returns) - 11): length(ts.brent.monthly.log.returns)],
-                     as.data.frame(ses.brent.monthly.log.returns)$`Point Forecast`)
-ses.results$mape[2] <- mape(ts.brent.monthly.difference[(length(ts.brent.monthly.difference) - 11): length(ts.brent.monthly.difference)],
-                       as.data.frame(ses.brent.monthly.difference)$`Point Forecast`)
-ses.results$mape[7] <- mape(ts.brent.monthly.log.returns[(length(ts.brent.monthly.log.returns) - 11): length(ts.brent.monthly.log.returns)],
-                       as.data.frame(ses.brent.monthly.log.returns)$`Point Forecast`)
 
-### Dubai
-ses.dubai.monthly.difference <- forecast::ses(ts.dubai.monthly.difference[1: (length(ts.dubai.monthly.difference) - 11)], h = 12)
-ses.dubai.monthly.log.returns <- forecast::ses(ts.dubai.monthly.log.returns[1: (length(ts.dubai.monthly.log.returns) - 11)], h = 12)
+### APSP log returns
+# Split data
+ts.train <- ts(ts.apsp.monthly.log.returns[1:(length(ts.apsp.monthly.log.returns) - 12)],
+               start = conv_to_ym(time(ts.apsp.monthly.log.returns)[1] - 0.083),
+               end = conv_to_ym(time(ts.apsp.monthly.log.returns)[(length(ts.apsp.monthly.log.returns) - 12)] - 0.083),
+               frequency = 12)
+ts.test <- ts(ts.apsp.monthly.log.returns[(length(ts.apsp.monthly.log.returns) - 11):length(ts.apsp.monthly.log.returns)],
+              start = conv_to_ym(time(ts.apsp.monthly.log.returns)[(length(ts.apsp.monthly.log.returns) - 11)] - 0.083),
+              end = conv_to_ym(time(ts.apsp.monthly.log.returns)[length(ts.apsp.monthly.log.returns)] - 0.083),
+              frequency = 12)
 
-ses.results$mae[3] <- mae(ts.dubai.monthly.difference[(length(ts.dubai.monthly.difference) - 11): length(ts.dubai.monthly.difference)],
-                     as.data.frame(ses.dubai.monthly.difference)$`Point Forecast`)
-ses.results$mae[8] <- mae(ts.dubai.monthly.log.returns[(length(ts.dubai.monthly.log.returns) - 11): length(ts.dubai.monthly.log.returns)],
-                     as.data.frame(ses.dubai.monthly.log.returns)$`Point Forecast`)
-ses.results$mape[3] <- mape(ts.dubai.monthly.difference[(length(ts.dubai.monthly.difference) - 11): length(ts.dubai.monthly.difference)],
-                       as.data.frame(ses.dubai.monthly.difference)$`Point Forecast`)
-ses.results$mape[8] <- mape(ts.dubai.monthly.log.returns[(length(ts.dubai.monthly.log.returns) - 11): length(ts.dubai.monthly.log.returns)],
-                       as.data.frame(ses.dubai.monthly.log.returns)$`Point Forecast`)
+# Fit SES model
+fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
+ses.results$alpha[6] <- unname(fit$model$par[1])
 
-### NatGas
-ses.natgas.us.monthly.difference <- forecast::ses(ts.natgas.us.monthly.difference[1: (length(ts.natgas.us.monthly.difference) - 11)], h = 12)
-ses.natgas.us.monthly.log.returns <- forecast::ses(ts.natgas.us.monthly.log.returns[1: (length(ts.natgas.us.monthly.log.returns) - 11)], h = 12)
+# Evaluate
+ses.results$mae[6] <- mae(ts.test, fit$mean)
+ses.results$mape[6] <- mape(ts.test, fit$mean)
 
-ses.results$mae[4] <- mae(ts.natgas.us.monthly.difference[(length(ts.natgas.us.monthly.difference) - 11): length(ts.natgas.us.monthly.difference)],
-                     as.data.frame(ses.natgas.us.monthly.difference)$`Point Forecast`)
-ses.results$mae[9] <- mae(ts.natgas.us.monthly.log.returns[(length(ts.natgas.us.monthly.log.returns) - 11): length(ts.natgas.us.monthly.log.returns)],
-                     as.data.frame(ses.natgas.us.monthly.log.returns)$`Point Forecast`)
-ses.results$mape[4] <- mape(ts.natgas.us.monthly.difference[(length(ts.natgas.us.monthly.difference) - 11): length(ts.natgas.us.monthly.difference)],
-                       as.data.frame(ses.natgas.us.monthly.difference)$`Point Forecast`)
-ses.results$mape[9] <- mape(ts.natgas.us.monthly.log.returns[(length(ts.natgas.us.monthly.log.returns) - 11): length(ts.natgas.us.monthly.log.returns)],
-                       as.data.frame(ses.natgas.us.monthly.log.returns)$`Point Forecast`)
 
-### WTI
-ses.wti.monthly.difference <- forecast::ses(ts.wti.monthly.difference[1: (length(ts.wti.monthly.difference) - 11)], h = 12)
-ses.wti.monthly.log.returns <- forecast::ses(ts.wti.monthly.log.returns[1: (length(ts.wti.monthly.log.returns) - 11)], h = 12)
+### Brent first difference
+# Split data
+ts.train <- ts(ts.brent.monthly.difference[1:(length(ts.brent.monthly.difference) - 12)],
+               start = conv_to_ym(time(ts.brent.monthly.difference)[1] - 0.083),
+               end = conv_to_ym(time(ts.brent.monthly.difference)[(length(ts.brent.monthly.difference) - 12)] - 0.083),
+               frequency = 12)
+ts.test <- ts(ts.brent.monthly.difference[(length(ts.brent.monthly.difference) - 11):length(ts.brent.monthly.difference)],
+              start = conv_to_ym(time(ts.brent.monthly.difference)[(length(ts.brent.monthly.difference) - 11)] - 0.083),
+              end = conv_to_ym(time(ts.brent.monthly.difference)[length(ts.brent.monthly.difference)] - 0.083),
+              frequency = 12)
 
-ses.results$mae[5] <- mae(ts.wti.monthly.difference[(length(ts.wti.monthly.difference) - 11): length(ts.wti.monthly.difference)],
-                     as.data.frame(ses.wti.monthly.difference)$`Point Forecast`)
-ses.results$mae[10] <- mae(ts.wti.monthly.log.returns[(length(ts.wti.monthly.log.returns) - 11): length(ts.wti.monthly.log.returns)],
-                     as.data.frame(ses.wti.monthly.log.returns)$`Point Forecast`)
-ses.results$mape[5] <- mape(ts.wti.monthly.difference[(length(ts.wti.monthly.difference) - 11): length(ts.wti.monthly.difference)],
-                       as.data.frame(ses.wti.monthly.difference)$`Point Forecast`)
-ses.results$mape[10] <- mape(ts.wti.monthly.log.returns[(length(ts.wti.monthly.log.returns) - 11): length(ts.wti.monthly.log.returns)],
-                       as.data.frame(ses.wti.monthly.log.returns)$`Point Forecast`)
+# Fit SES model
+fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
+ses.results$alpha[2] <- unname(fit$model$par[1])
 
+# Evaluate
+ses.results$mae[2] <- mae(ts.test, fit$mean)
+ses.results$mape[2] <- mape(ts.test, fit$mean)
+
+
+### Brent log returns
+# Split data
+ts.train <- ts(ts.brent.monthly.log.returns[1:(length(ts.brent.monthly.log.returns) - 12)],
+               start = conv_to_ym(time(ts.brent.monthly.log.returns)[1] - 0.083),
+               end = conv_to_ym(time(ts.brent.monthly.log.returns)[(length(ts.brent.monthly.log.returns) - 12)] - 0.083),
+               frequency = 12)
+ts.test <- ts(ts.brent.monthly.log.returns[(length(ts.brent.monthly.log.returns) - 11):length(ts.brent.monthly.log.returns)],
+              start = conv_to_ym(time(ts.brent.monthly.log.returns)[(length(ts.brent.monthly.log.returns) - 11)] - 0.083),
+              end = conv_to_ym(time(ts.brent.monthly.log.returns)[length(ts.brent.monthly.log.returns)] - 0.083),
+              frequency = 12)
+
+# Fit SES model
+fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
+ses.results$alpha[7] <- unname(fit$model$par[1])
+
+# Evaluate
+ses.results$mae[7] <- mae(ts.test, fit$mean)
+ses.results$mape[7] <- mape(ts.test, fit$mean)
+
+
+### Dubai first difference
+# Split data
+ts.train <- ts(ts.dubai.monthly.difference[1:(length(ts.dubai.monthly.difference) - 12)],
+               start = conv_to_ym(time(ts.dubai.monthly.difference)[1] - 0.083),
+               end = conv_to_ym(time(ts.dubai.monthly.difference)[(length(ts.dubai.monthly.difference) - 12)] - 0.083),
+               frequency = 12)
+ts.test <- ts(ts.dubai.monthly.difference[(length(ts.dubai.monthly.difference) - 11):length(ts.dubai.monthly.difference)],
+              start = conv_to_ym(time(ts.dubai.monthly.difference)[(length(ts.dubai.monthly.difference) - 11)] - 0.083),
+              end = conv_to_ym(time(ts.dubai.monthly.difference)[length(ts.dubai.monthly.difference)] - 0.083),
+              frequency = 12)
+
+# Fit SES model
+fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
+ses.results$alpha[3] <- unname(fit$model$par[1])
+
+# Evaluate
+ses.results$mae[3] <- mae(ts.test, fit$mean)
+ses.results$mape[3] <- mape(ts.test, fit$mean)
+
+
+### Dubai log returns
+# Split data
+ts.train <- ts(ts.dubai.monthly.log.returns[1:(length(ts.dubai.monthly.log.returns) - 12)],
+               start = conv_to_ym(time(ts.dubai.monthly.log.returns)[1] - 0.083),
+               end = conv_to_ym(time(ts.dubai.monthly.log.returns)[(length(ts.dubai.monthly.log.returns) - 12)] - 0.083),
+               frequency = 12)
+ts.test <- ts(ts.dubai.monthly.log.returns[(length(ts.dubai.monthly.log.returns) - 11):length(ts.dubai.monthly.log.returns)],
+              start = conv_to_ym(time(ts.dubai.monthly.log.returns)[(length(ts.dubai.monthly.log.returns) - 11)] - 0.083),
+              end = conv_to_ym(time(ts.dubai.monthly.log.returns)[length(ts.dubai.monthly.log.returns)] - 0.083),
+              frequency = 12)
+
+# Fit SES model
+fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
+ses.results$alpha[8] <- unname(fit$model$par[1])
+
+# Evaluate
+ses.results$mae[8] <- mae(ts.test, fit$mean)
+ses.results$mape[8] <- mape(ts.test, fit$mean)
+
+
+### NatGas first difference
+# Split data
+ts.train <- ts(ts.natgas.us.monthly.difference[1:(length(ts.natgas.us.monthly.difference) - 12)],
+               start = conv_to_ym(time(ts.natgas.us.monthly.difference)[1] - 0.083),
+               end = conv_to_ym(time(ts.natgas.us.monthly.difference)[(length(ts.natgas.us.monthly.difference) - 12)] - 0.083),
+               frequency = 12)
+ts.test <- ts(ts.natgas.us.monthly.difference[(length(ts.natgas.us.monthly.difference) - 11):length(ts.natgas.us.monthly.difference)],
+              start = conv_to_ym(time(ts.natgas.us.monthly.difference)[(length(ts.natgas.us.monthly.difference) - 11)] - 0.083),
+              end = conv_to_ym(time(ts.natgas.us.monthly.difference)[length(ts.natgas.us.monthly.difference)] - 0.083),
+              frequency = 12)
+
+# Fit SES model
+fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
+ses.results$alpha[4] <- unname(fit$model$par[1])
+
+# Evaluate
+ses.results$mae[4] <- mae(ts.test, fit$mean)
+ses.results$mape[4] <- mape(ts.test, fit$mean)
+
+
+### NatGas log returns
+# Split data
+ts.train <- ts(ts.natgas.us.monhtly.log.returns[1:(length(ts.natgas.us.monhtly.log.returns) - 12)],
+               start = conv_to_ym(time(ts.natgas.us.monhtly.log.returns)[1] - 0.083),
+               end = conv_to_ym(time(ts.natgas.us.monhtly.log.returns)[(length(ts.natgas.us.monhtly.log.returns) - 12)] - 0.083),
+               frequency = 12)
+ts.test <- ts(ts.natgas.us.monhtly.log.returns[(length(ts.natgas.us.monhtly.log.returns) - 11):length(ts.natgas.us.monhtly.log.returns)],
+              start = conv_to_ym(time(ts.natgas.us.monhtly.log.returns)[(length(ts.natgas.us.monhtly.log.returns) - 11)] - 0.083),
+              end = conv_to_ym(time(ts.natgas.us.monhtly.log.returns)[length(ts.natgas.us.monhtly.log.returns)] - 0.083),
+              frequency = 12)
+
+# Fit SES model
+fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
+ses.results$alpha[9] <- unname(fit$model$par[1])
+
+# Evaluate
+ses.results$mae[9] <- mae(ts.test, fit$mean)
+ses.results$mape[9] <- mape(ts.test, fit$mean)
+
+
+### WTI first difference
+# Split data
+ts.train <- ts(ts.wti.monthly.difference[1:(length(ts.wti.monthly.difference) - 12)],
+               start = conv_to_ym(time(ts.wti.monthly.difference)[1] - 0.083),
+               end = conv_to_ym(time(ts.wti.monthly.difference)[(length(ts.wti.monthly.difference) - 12)] - 0.083),
+               frequency = 12)
+ts.test <- ts(ts.wti.monthly.difference[(length(ts.wti.monthly.difference) - 11):length(ts.wti.monthly.difference)],
+              start = conv_to_ym(time(ts.wti.monthly.difference)[(length(ts.wti.monthly.difference) - 11)] - 0.083),
+              end = conv_to_ym(time(ts.wti.monthly.difference)[length(ts.wti.monthly.difference)] - 0.083),
+              frequency = 12)
+
+# Fit SES model
+fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
+ses.results$alpha[5] <- unname(fit$model$par[1])
+
+# Evaluate
+ses.results$mae[5] <- mae(ts.test, fit$mean)
+ses.results$mape[5] <- mape(ts.test, fit$mean)
+
+
+### NatGas log returns
+# Split data
+ts.train <- ts(ts.wti.monthly.log.returns[1:(length(ts.wti.monthly.log.returns) - 12)],
+               start = conv_to_ym(time(ts.wti.monthly.log.returns)[1] - 0.083),
+               end = conv_to_ym(time(ts.wti.monthly.log.returns)[(length(ts.wti.monthly.log.returns) - 12)] - 0.083),
+               frequency = 12)
+ts.test <- ts(ts.wti.monthly.log.returns[(length(ts.wti.monthly.log.returns) - 11):length(ts.wti.monthly.log.returns)],
+              start = conv_to_ym(time(ts.wti.monthly.log.returns)[(length(ts.wti.monthly.log.returns) - 11)] - 0.083),
+              end = conv_to_ym(time(ts.wti.monthly.log.returns)[length(ts.wti.monthly.log.returns)] - 0.083),
+              frequency = 12)
+
+# Fit SES model
+fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
+ses.results$alpha[10] <- unname(fit$model$par[1])
+
+# Evaluate
+ses.results$mae[10] <- mae(ts.test, fit$mean)
+ses.results$mape[10] <- mape(ts.test, fit$mean)
 
 ### Save the results
 save(ses.results, file = "results/econometric models/SES.RData")
