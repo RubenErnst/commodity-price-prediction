7cf75ceb042d1ad65193b9be73d710a27494b7f1
Ruben Ernst <57605025+RubenErnst@users.noreply.github.com>
2021-09-28 08:08:23 +0200

    Create 1 descriptive stats.R

---
+++ code/1 descriptive stats.R
@@ -0,0 +1,260 @@
+rm(list = ls())
+
+library(tidyverse)
+library(moments)
+
+
+load("clean data/timeseries.RData")
+
+### APSP
+# Assemble table of descriptive stats
+apsp.desc <- data.frame("price" = ts.apsp.monthly.absolute[1:length(ts.apsp.monthly.absolute)],
+                        "year" = lubridate::year(zoo::yearmon(time(ts.apsp.monthly.absolute))),
+                        "month" = lubridate::month(zoo::yearmon(time(ts.apsp.monthly.absolute))))
+
+apsp.desc <- merge(select(aggregate(price ~ year, apsp.desc, quantile, 0), "quantile.0" = price, year),
+                   select(aggregate(price ~ year, apsp.desc, quantile, 0.25), "quantile.25" = price, year),
+                   by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, apsp.desc, quantile, 0.5),
+                  "quantile.50" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, apsp.desc, quantile, 0.75),
+                  "quantile.75" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, apsp.desc, quantile, 1),
+                  "quantile.100" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, apsp.desc, mean),
+                  "mean" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, apsp.desc, median),
+                  "median" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, apsp.desc, function(x){unique(x)[which.max(tabulate(match(x, unique(x))))]}),
+                  "mode" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, apsp.desc, var),
+                  "variance" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, apsp.desc, sd),
+                  "sd" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, apsp.desc, skewness),
+                  "skewness" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, apsp.desc, kurtosis),
+                  "kurtosis" = price, year),
+        by = "year", all.x = TRUE)
+
+apsp.desc$commodity <- "APSP"
+
+
+### Brent
+# Assemble table of descriptive stats
+brent.desc <- data.frame("price" = ts.brent.monthly.absolute[1:length(ts.brent.monthly.absolute)],
+                         "year" = lubridate::year(zoo::yearmon(time(ts.brent.monthly.absolute))),
+                         "month" = lubridate::month(zoo::yearmon(time(ts.brent.monthly.absolute))))
+
+brent.desc <- merge(select(aggregate(price ~ year, brent.desc, quantile, 0), "quantile.0" = price, year),
+                    select(aggregate(price ~ year, brent.desc, quantile, 0.25), "quantile.25" = price, year),
+                    by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, brent.desc, quantile, 0.5),
+                  "quantile.50" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, brent.desc, quantile, 0.75),
+                  "quantile.75" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, brent.desc, quantile, 1),
+                  "quantile.100" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, brent.desc, mean),
+                  "mean" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, brent.desc, median),
+                  "median" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, brent.desc, function(x){unique(x)[which.max(tabulate(match(x, unique(x))))]}),
+                  "mode" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, brent.desc, var),
+                  "variance" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, brent.desc, sd),
+                  "sd" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, brent.desc, skewness),
+                  "skewness" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, brent.desc, kurtosis),
+                  "kurtosis" = price, year),
+        by = "year", all.x = TRUE)
+
+brent.desc$commodity <- "Brent"
+
+
+### Dubai
+# Assemble table of descriptive stats
+dubai.desc <- data.frame("price" = ts.dubai.monthly.absolute[1:length(ts.dubai.monthly.absolute)],
+                         "year" = lubridate::year(zoo::yearmon(time(ts.dubai.monthly.absolute))),
+                         "month" = lubridate::month(zoo::yearmon(time(ts.dubai.monthly.absolute))))
+
+dubai.desc <- merge(select(aggregate(price ~ year, dubai.desc, quantile, 0), "quantile.0" = price, year),
+                    select(aggregate(price ~ year, dubai.desc, quantile, 0.25), "quantile.25" = price, year),
+                    by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, dubai.desc, quantile, 0.5),
+                  "quantile.50" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, dubai.desc, quantile, 0.75),
+                  "quantile.75" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, dubai.desc, quantile, 1),
+                  "quantile.100" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, dubai.desc, mean),
+                  "mean" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, dubai.desc, median),
+                  "median" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, dubai.desc, function(x){unique(x)[which.max(tabulate(match(x, unique(x))))]}),
+                  "mode" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, dubai.desc, var),
+                  "variance" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, dubai.desc, sd),
+                  "sd" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, dubai.desc, skewness),
+                  "skewness" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, dubai.desc, kurtosis),
+                  "kurtosis" = price, year),
+        by = "year", all.x = TRUE)
+
+dubai.desc$commodity <- "Dubai"
+
+
+### NatGas
+# Assemble table of descriptive stats
+natgas.us.desc <- data.frame("price" = ts.natgas.us.monthly.absolute[1:length(ts.natgas.us.monthly.absolute)],
+                             "year" = lubridate::year(zoo::yearmon(time(ts.natgas.us.monthly.absolute))),
+                             "month" = lubridate::month(zoo::yearmon(time(ts.natgas.us.monthly.absolute))))
+
+natgas.us.desc <- merge(select(aggregate(price ~ year, natgas.us.desc, quantile, 0), "quantile.0" = price, year),
+                        select(aggregate(price ~ year, natgas.us.desc, quantile, 0.25), "quantile.25" = price, year),
+                        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, natgas.us.desc, quantile, 0.5),
+                  "quantile.50" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, natgas.us.desc, quantile, 0.75),
+                  "quantile.75" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, natgas.us.desc, quantile, 1),
+                  "quantile.100" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, natgas.us.desc, mean),
+                  "mean" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, natgas.us.desc, median),
+                  "median" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, natgas.us.desc, function(x){unique(x)[which.max(tabulate(match(x, unique(x))))]}),
+                  "mode" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, natgas.us.desc, var),
+                  "variance" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, natgas.us.desc, sd),
+                  "sd" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, natgas.us.desc, skewness),
+                  "skewness" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, natgas.us.desc, kurtosis),
+                  "kurtosis" = price, year),
+        by = "year", all.x = TRUE)
+
+natgas.us.desc$commodity <- "NatGas"
+
+
+### WTI
+# Assemble table of descriptive stats
+wti.desc <- data.frame("price" = ts.wti.monthly.absolute[1:length(ts.wti.monthly.absolute)],
+                       "year" = lubridate::year(zoo::yearmon(time(ts.wti.monthly.absolute))),
+                       "month" = lubridate::month(zoo::yearmon(time(ts.wti.monthly.absolute))))
+
+wti.desc <- merge(select(aggregate(price ~ year, wti.desc, quantile, 0), "quantile.0" = price, year),
+                  select(aggregate(price ~ year, wti.desc, quantile, 0.25), "quantile.25" = price, year),
+                  by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, wti.desc, quantile, 0.5),
+                  "quantile.50" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, wti.desc, quantile, 0.75),
+                  "quantile.75" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, wti.desc, quantile, 1),
+                  "quantile.100" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, wti.desc, mean),
+                  "mean" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, wti.desc, median),
+                  "median" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, wti.desc, function(x){unique(x)[which.max(tabulate(match(x, unique(x))))]}),
+                  "mode" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, wti.desc, var),
+                  "variance" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, wti.desc, sd),
+                  "sd" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, wti.desc, skewness),
+                  "skewness" = price, year),
+        by = "year", all.x = TRUE) %>%
+  merge(., select(aggregate(price ~ year, wti.desc, kurtosis),
+                  "kurtosis" = price, year),
+        by = "year", all.x = TRUE)
+
+wti.desc$commodity <- "WTI"
+
+
+# Save results
+descriptives <- rbind(apsp.desc, brent.desc, dubai.desc, natgas.us.desc, wti.desc)
+save(descriptives, file = "results/descriptive stats/Descriptives.RData")
+
+
+##### Graphs ----
+### Boxplots
+boxplot.data <- data.frame("price" = c(ts.apsp.monthly.absolute[1:length(ts.apsp.monthly.absolute)],
+                                       ts.brent.monthly.absolute[1:length(ts.brent.monthly.absolute)],
+                                       ts.dubai.monthly.absolute[1:length(ts.dubai.monthly.absolute)],
+                                       ts.natgas.us.monthly.absolute[1:length(ts.natgas.us.monthly.absolute)],
+                                       ts.wti.monthly.absolute[1:length(ts.wti.monthly.absolute)]),
+                           "year" = c(lubridate::year(zoo::yearmon(time(ts.apsp.monthly.absolute))),
+                                      lubridate::year(zoo::yearmon(time(ts.brent.monthly.absolute))),
+                                      lubridate::year(zoo::yearmon(time(ts.dubai.monthly.absolute))),
+                                      lubridate::year(zoo::yearmon(time(ts.natgas.us.monthly.absolute))),
+                                      lubridate::year(zoo::yearmon(time(ts.wti.monthly.absolute)))),
+                           "commodity" = c(rep("APSP", length(ts.apsp.monthly.absolute)),
+                                           rep("Brent", length(ts.brent.monthly.absolute)),
+                                           rep("Dubai", length(ts.dubai.monthly.absolute)),
+                                           rep("NatGas", length(ts.natgas.us.monthly.absolute)),
+                                           rep("WTI", length(ts.wti.monthly.absolute))))
+
+boxplot.data$year <- as.factor(boxplot.data$year)
+
+dispersion.plot <- ggplot(data = boxplot.data, aes(x = year, y = price)) +
+  geom_boxplot() +
+  labs(title = "Descriptive statistics: dispersion per commodity", x = "Year", y = "Price (USD)") +
+  theme_bw() +
+  theme(panel.grid = element_blank(),
+        axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+  facet_wrap( ~ commodity, scales = "free",
+             labeller = as_labeller(c("APSP" = "APSP", "Brent" = "Brent", "Dubai" = "Dubai", "NatGas" = "NatGas Henry Hub", "WTI" = "WTI")))
+
+dispersion.plot
+
+ggsave("plots/1 Price dispersion plot.pdf", dispersion.plot, device = cairo_pdf, width = 12, height = 9)
