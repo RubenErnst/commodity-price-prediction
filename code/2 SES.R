ae960fb7fa435be3c69a12bc10da175fc81e6437
Ruben Ernst <57605025+RubenErnst@users.noreply.github.com>
2021-10-22 15:11:12 +0200

    Update 2 SES.R

--- code/2 SES.R
+++ code/2 SES.R
@@ -27,11 +27,11 @@ conv_to_ym <- function(ts_date){
 
 
 # Resulting data frame
-ses.results <- data.frame("commodity" = rep(c("APSP", "Brent", "Dubai", "US NatGas", "WTI"), 2),
-                          "series" = c(rep("difference", 5), rep("log returns", 5)),
-                          "alpha" = rep(NA, 10),
-                          "mae" = rep(NA, 10),
-                          "mape" = rep(NA, 10))
+ses.results <- data.frame("commodity" = rep(c("APSP", "Brent", "Dubai", "LNG", "US NatGas", "WTI"), 2),
+                          "series" = c(rep("difference", 6), rep("log returns", 6)),
+                          "alpha" = rep(NA, 12),
+                          "mae" = rep(NA, 12),
+                          "mape" = rep(NA, 12))
 
 ### APSP first difference
 # Split data
@@ -60,12 +60,12 @@ ts.train <- log(ts.apsp.monthly.absolute[1:length(ts.apsp.monthly.absolute)] / l
 
 # Fit SES model
 fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
-ses.results$alpha[6] <- unname(fit$model$par[1])
+ses.results$alpha[7] <- unname(fit$model$par[1])
 
 # Evaluate
 apsp.pred.log.return <- ts.apsp.monthly.absolute[length(ts.apsp.monthly.absolute) - 12] * cumprod(exp(fit$mean))
-ses.results$mae[6] <- mae(ts.test, apsp.pred.log.return)
-ses.results$mape[6] <- mape(ts.test, apsp.pred.log.return)
+ses.results$mae[7] <- mae(ts.test, apsp.pred.log.return)
+ses.results$mape[7] <- mape(ts.test, apsp.pred.log.return)
 
 
 ### Brent first difference
@@ -95,12 +95,12 @@ ts.train <- log(ts.brent.monthly.absolute[1:length(ts.brent.monthly.absolute)] /
 
 # Fit SES model
 fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
-ses.results$alpha[7] <- unname(fit$model$par[1])
+ses.results$alpha[8] <- unname(fit$model$par[1])
 
 # Evaluate
 brent.pred.log.return <- ts.brent.monthly.absolute[length(ts.brent.monthly.absolute) - 12] * cumprod(exp(fit$mean))
-ses.results$mae[7] <- mae(ts.test, brent.pred.log.return)
-ses.results$mape[7] <- mape(ts.test, brent.pred.log.return)
+ses.results$mae[8] <- mae(ts.test, brent.pred.log.return)
+ses.results$mape[8] <- mape(ts.test, brent.pred.log.return)
 
 
 ### Dubai first difference
@@ -130,12 +130,47 @@ ts.train <- log(ts.dubai.monthly.absolute[1:length(ts.dubai.monthly.absolute)] /
 
 # Fit SES model
 fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
-ses.results$alpha[8] <- unname(fit$model$par[1])
+ses.results$alpha[9] <- unname(fit$model$par[1])
 
 # Evaluate
 dubai.pred.log.return <- ts.dubai.monthly.absolute[length(ts.dubai.monthly.absolute) - 12] * cumprod(exp(fit$mean))
-ses.results$mae[8] <- mae(ts.test, dubai.pred.log.return)
-ses.results$mape[8] <- mape(ts.test, dubai.pred.log.return)
+ses.results$mae[9] <- mae(ts.test, dubai.pred.log.return)
+ses.results$mape[9] <- mape(ts.test, dubai.pred.log.return)
+
+
+### LNG first difference
+# Split data
+ts.train <- ts(ts.lng.monthly.difference[1:(length(ts.lng.monthly.difference) - 12)],
+               start = conv_to_ym(time(ts.lng.monthly.difference)[1] - 0.083),
+               end = conv_to_ym(time(ts.lng.monthly.difference)[(length(ts.lng.monthly.difference) - 12)] - 0.083),
+               frequency = 12)
+ts.test <- ts(ts.lng.monthly.absolute[(length(ts.lng.monthly.absolute) - 11):length(ts.lng.monthly.absolute)],
+              start = conv_to_ym(time(ts.lng.monthly.absolute)[(length(ts.lng.monthly.absolute) - 11)] - 0.083),
+              end = conv_to_ym(time(ts.lng.monthly.absolute)[length(ts.lng.monthly.absolute)] - 0.083),
+              frequency = 12)
+
+# Fit SES model
+fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
+ses.results$alpha[4] <- unname(fit$model$par[1])
+
+# Evaluate
+lng.pred.diff <- cumsum(c(ts.lng.monthly.absolute[length(ts.lng.monthly.absolute) - 12], fit$mean))[-1]
+ses.results$mae[4] <- mae(ts.test, lng.pred.diff)
+ses.results$mape[4] <- mape(ts.test, lng.pred.diff)
+
+
+### LNG log returns
+# Split data
+ts.train <- log(ts.lng.monthly.absolute[1:length(ts.lng.monthly.absolute)] / lag(ts.lng.monthly.absolute[1:length(ts.lng.monthly.absolute)]))[2:(length(ts.lng.monthly.absolute) - 12)]
+
+# Fit SES model
+fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
+ses.results$alpha[10] <- unname(fit$model$par[1])
+
+# Evaluate
+lng.pred.log.return <- ts.lng.monthly.absolute[length(ts.lng.monthly.absolute) - 12] * cumprod(exp(fit$mean))
+ses.results$mae[10] <- mae(ts.test, lng.pred.log.return)
+ses.results$mape[10] <- mape(ts.test, lng.pred.log.return)
 
 
 ### NatGas first difference
@@ -151,12 +186,12 @@ ts.test <- ts(ts.natgas.us.monthly.absolute[(length(ts.natgas.us.monthly.absolut
 
 # Fit SES model
 fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
-ses.results$alpha[4] <- unname(fit$model$par[1])
+ses.results$alpha[5] <- unname(fit$model$par[1])
 
 # Evaluate
 natgas.us.pred.diff <- cumsum(c(ts.natgas.us.monthly.absolute[length(ts.natgas.us.monthly.absolute) - 12], fit$mean))[-1]
-ses.results$mae[4] <- mae(ts.test, natgas.us.pred.diff)
-ses.results$mape[4] <- mape(ts.test, natgas.us.pred.diff)
+ses.results$mae[5] <- mae(ts.test, natgas.us.pred.diff)
+ses.results$mape[5] <- mape(ts.test, natgas.us.pred.diff)
 
 
 ### NatGas log returns
@@ -165,12 +200,12 @@ ts.train <- log(ts.natgas.us.monthly.absolute[1:length(ts.natgas.us.monthly.abso
 
 # Fit SES model
 fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
-ses.results$alpha[9] <- unname(fit$model$par[1])
+ses.results$alpha[11] <- unname(fit$model$par[1])
 
 # Evaluate
 natgas.us.pred.log.return<- ts.natgas.us.monthly.absolute[length(ts.natgas.us.monthly.absolute) - 12] * cumprod(exp(fit$mean))
-ses.results$mae[9] <- mae(ts.test, natgas.us.pred.log.return)
-ses.results$mape[9] <- mape(ts.test, natgas.us.pred.log.return)
+ses.results$mae[11] <- mae(ts.test, natgas.us.pred.log.return)
+ses.results$mape[11] <- mape(ts.test, natgas.us.pred.log.return)
 
 
 ### WTI first difference
@@ -186,12 +221,12 @@ ts.test <- ts(ts.wti.monthly.absolute[(length(ts.wti.monthly.absolute) - 11):len
 
 # Fit SES model
 fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
-ses.results$alpha[5] <- unname(fit$model$par[1])
+ses.results$alpha[6] <- unname(fit$model$par[1])
 
 # Evaluate
 wti.pred.diff <- cumsum(c(ts.wti.monthly.absolute[length(ts.wti.monthly.absolute) - 12], fit$mean))[-1]
-ses.results$mae[5] <- mae(ts.test, wti.pred.diff)
-ses.results$mape[5] <- mape(ts.test, wti.pred.diff)
+ses.results$mae[6] <- mae(ts.test, wti.pred.diff)
+ses.results$mape[6] <- mape(ts.test, wti.pred.diff)
 
 
 ### WTI log returns
@@ -200,12 +235,12 @@ ts.train <- log(ts.wti.monthly.absolute[1:length(ts.wti.monthly.absolute)] / lag
 
 # Fit SES model
 fit <- ses(ts.train, h = 12) # h determines the number of periods to be forecast
-ses.results$alpha[10] <- unname(fit$model$par[1])
+ses.results$alpha[12] <- unname(fit$model$par[1])
 
 # Evaluate
 wti.pred.log.return <- ts.wti.monthly.absolute[length(ts.wti.monthly.absolute) - 12] * cumprod(exp(fit$mean))
-ses.results$mae[10] <- mae(ts.test, wti.pred.log.return)
-ses.results$mape[10] <- mape(ts.test, wti.pred.log.return)
+ses.results$mae[12] <- mae(ts.test, wti.pred.log.return)
+ses.results$mape[12] <- mape(ts.test, wti.pred.log.return)
 
 
 ### Save the results
@@ -215,6 +250,7 @@ openxlsx::write.xlsx(ses.results, file = "results/econometric models/SES.xlsx")
 ses.pred <- data.frame("apsp.diff" = apsp.pred.diff, "apsp.log.return" = apsp.pred.log.return,
                        "brent.diff" = brent.pred.diff, "brent.log.return" = brent.pred.log.return,
                        "dubai.diff" = dubai.pred.diff, "dubai.log.return" = dubai.pred.log.return,
+                       "lng.diff" = lng.pred.diff, "lng.log.return" = lng.pred.log.return,
                        "natgas.us.diff" = natgas.us.pred.diff, "natgas.us.log.return" = natgas.us.pred.log.return,
                        "wti.diff" = wti.pred.diff, "wti.log.return" = wti.pred.log.return)
 save(ses.pred, file = "results/econometric models/SES pred.Rdata")
