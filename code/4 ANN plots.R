da3ded14b4cd9b44f9d117cce4f76b63d5fb1109
Ruben Ernst <57605025+RubenErnst@users.noreply.github.com>
2021-11-07 10:51:37 +0100

    Update 4 ANN plots.R

--- code/4 ANN plots.R
+++ code/4 ANN plots.R
@@ -1,11 +1,57 @@
 rm(list = ls())
 
-library(devtools)
 library(tidyverse)
-source_url('https://gist.githubusercontent.com/fawda123/7471137/raw/466c1474d0a505ff044412703516c34f1a4684a5/nnet_plot_update.r')
+library(neuralnet)
 
 load("results/ml models/ANN pred.RData")
 
 
-### Plot
-plot.nnet(best.ann.apsp, x.lab = c("lag 1", "lag 2", "lag 3", "lag 4", "lag 5", "lag 6"), circle.col = list("black", "gray", "white", "red"))
+# Plot one neural net per commodity
+best.ann.apsp$model.list[["variables"]] <- paste0("lag", 1:length(best.ann.apsp$model.list[["variables"]]), sep = "")
+cairo_pdf("plots/5_APSP_ANN_plot.pdf")
+plot(best.ann.apsp, show.weights = FALSE, rep = "best", arrow.length = 0.16, dimension = 8)
+dev.off()
+
+best.ann.brent$model.list[["variables"]] <- paste0("lag", 1:length(best.ann.brent$model.list[["variables"]]), sep = "")
+cairo_pdf("plots/5_Brent_ANN_plot.pdf")
+plot(best.ann.brent, show.weights = FALSE, rep = "best", arrow.length = 0.16, dimension = 8)
+dev.off()
+
+best.ann.dubai$model.list[["variables"]] <- paste0("lag", 1:length(best.ann.dubai$model.list[["variables"]]), sep = "")
+cairo_pdf("plots/5_Dubai_ANN_plot.pdf")
+plot(best.ann.dubai, show.weights = FALSE, rep = "best", arrow.length = 0.16, dimension = 8)
+dev.off()
+
+best.ann.lng$model.list[["variables"]] <- paste0("lag", 1:length(best.ann.lng$model.list[["variables"]]), sep = "")
+cairo_pdf("plots/5_LNG_ANN_plot.pdf")
+plot(best.ann.lng, show.weights = FALSE, rep = "best", arrow.length = 0.16, dimension = 8)
+dev.off()
+
+best.ann.natgas.us$model.list[["variables"]] <- paste0("lag", 1:length(best.ann.natgas.us$model.list[["variables"]]), sep = "")
+cairo_pdf("plots/5_NatGas_ANN_plot.pdf")
+plot(best.ann.natgas.us, show.weights = FALSE, rep = "best", arrow.length = 0.16, dimension = 8)
+dev.off()
+
+best.ann.wti$model.list[["variables"]] <- paste0("lag", 1:length(best.ann.wti$model.list[["variables"]]), sep = "")
+cairo_pdf("plots/5_WTI_ANN_plot.pdf")
+plot(best.ann.wti, show.weights = FALSE, rep = "best", arrow.length = 0.16, dimension = 8)
+dev.off()
+
+
+# Plot all of them together
+cairo_pdf("plots/5_All_ANN_plots.pdf", width = 12, height = 8)
+layout(mat = matrix(data = 1:12, nrow = 4, ncol = 3, byrow = TRUE), heights = c(1, 5, 1, 5))
+par(mar=c(0, 0, 0, 0))
+plot.new()
+text(0.5, 0.8, "APSP", cex = 1.5, font = 2)
+plot.new()
+text(0.5, 0.8, "Brent", cex = 1.5, font = 2)
+plot.new()
+text(0.5, 0.8, "Dubai Fateh", cex = 1.5, font = 2)
+plot.new()
+text(0.5, 0, "LNG Asia", cex = 1.5, font = 2)
+plot.new()
+text(0.5, 0, "NatGas US", cex = 1.5, font = 2)
+plot.new()
+text(0.5, 0, "WTI", cex = 1.5, font = 2)
+dev.off()
